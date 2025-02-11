01c6a9af0c204c190a8da5f7d1795ea44648de62
Xavier Beatrice <58835993+Xavbeat03@users.noreply.github.com>
2025-02-10 23:44:35 -0500

    feat: some work done

--- main.rs
+++ main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use doc_comment::module;
+
+// Print all items in the threading module.
+for item in module!(std::threading).items() {
+    println!("{}", item);
 }
